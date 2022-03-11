84c543ec125531afaac78e457793d62dfd953028
Tristan Alonso <talonso09@ieszaidinvergeles.org>
2022-03-11 08:50:32 +0100

    Depuracion, refactorizacion y correccion de errores

--- Refactorizacion/src/refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/refactorizacion/Refactorizacion.java
@@ -14,18 +14,18 @@ public class Refactorizacion {
     /**
      * @param args the command line arguments
      */
-public static void main(String[] args) {
+    public static void main(String[] args) {
 // TODO code application logic here
-int []v= new int[5];
-v[0]=2;
-v[1]=4;
-v[2]=6;
-v[3]=8;
-Prueba p=new Prueba();
-p.va=10;
-p.mo(v);
-p.bo(v);
-p.in(v);
-}
-    
+        int[] v = new int[5];
+        v[0] = 2;
+        v[1] = 4;
+        v[2] = 6;
+        v[3] = 8;
+        Prueba p = new Prueba();
+        p.va = 10;
+        p.mo(v);
+        p.bo(v);
+        p.in(v);
+    }
+
 }
