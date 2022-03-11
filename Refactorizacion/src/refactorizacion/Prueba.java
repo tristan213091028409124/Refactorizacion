65efc228a2b0b6a059703604d0a67a2747bbe228
Tristan Alonso <talonso09@ieszaidinvergeles.org>
2022-03-11 09:34:02 +0100

    Ejercicio 3 cambiar parámetros

--- Refactorizacion/src/refactorizacion/Prueba.java
+++ Refactorizacion/src/refactorizacion/Prueba.java
@@ -6,6 +6,7 @@ public class Prueba {
 //metodo que modifica el contenido de la posicion posicion de un array con el valor pasado
 
     public void modificar(int[] vector) {
+        int LongitudActual = 0;
         int i;
         int posicion = 1;
 //Mostramos los elementos del array
@@ -30,6 +31,7 @@ vector[posicion] = this.valor;
 //metodo que borra el elemento de la posicion posicion desplazando a la izquierda todos los elementos
 
     public void borrar(int[] v) {
+        int LongitudActual = 0;
         int p = 1, i;
 //mostramos su contenido
 //Mostramos los elementos del array
@@ -55,6 +57,7 @@ vector[posicion] = this.valor;
 //metodo que inserta un elemento en la posicion posicion desplazando a la derecha todos los elementos
 
     public void insertar(int[] v) {
+        int LongitudActual = 0;
         int p = 1, i;
 //mostramos su contenido
 //Mostramos los elementos del array
